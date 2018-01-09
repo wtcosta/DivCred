c1e09e0ed15e7019c8834606d10f5d3b69173a5a
Wesley <contato@wesleyteixeira.com.br>
2018-01-09 19:06:36 -0300

    Roteando index inicial para login e remoção da view index inicial / Remoção menu para users ñ logado

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -1,5 +1,11 @@
 <?php
 class IndexController extends \HXPHP\System\Controller
 {
-    
+	public function __construct($configs)
+	{
+		parent::__construct($configs);
+
+		$this->view->setPath('login')
+					->setFile('index');
+	}
 }
